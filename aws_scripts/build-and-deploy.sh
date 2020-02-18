01d79287acf7d01c21725d029ce4c4bd44b6404b
Gabriel Totev <gttotev8@gmail.com>
2020-02-17 17:57:36 -0800

    Add Makefile for easy ops

--- aws_scripts/build-and-deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-[ -f .catkin_workspace ] || { echo not in correct directory!; exit 1; }
-
-colcon build
-colcon bundle
-
-../aws_scripts/update-sim-app.sh
-../aws_scripts/create-sim-job.sh $1
