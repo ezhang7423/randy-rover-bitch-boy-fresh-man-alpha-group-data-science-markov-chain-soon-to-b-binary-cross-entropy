0860d5169c67db9f250b1f88717f94de48c5f072
ezhang7423 <ezhang7423@student.palomar.edu>
2020-03-13 07:32:08 +0000

    Add additional dependencies in setup.sh

---
+++ simulation_ws/scripts/fix-ignition.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sed -i 's/api\.ignitionfuel\.org/api.ignitionrobotics.org/' $HOME/.ignition/fuel/config.yaml
--- simulation_ws/scripts/setup.sh
+++ simulation_ws/scripts/setup.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
+[ -z "$VIRTUAL_ENV" ] && { echo Must run from venv! Create one in repo root and activate.; exit 2; }
 if ! which roslaunch &> /dev/null; then
-	sudo add-apt-repository universe multiverse restricted
-	sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-	sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
-	sudo apt-get update
-	sudo apt-get install ros-melodic-desktop-full
-	sudo rosdep init
-	echo "source /opt/ros/melodic/setup.bash" >> $HOME/.bashrc
-        echo '##############################################'
-        echo '##############################################'
-        echo ROS installed! Source ~/.bashrc to get in PATH.
-        echo '##############################################'
-        echo '##############################################'
+    sudo add-apt-repository universe multiverse restricted
+    sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+    sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+    sudo apt-get update
+    sudo apt-get install ros-melodic-desktop-full
+    sudo rosdep init
+    echo "source /opt/ros/melodic/setup.bash" >> $HOME/.bashrc
+    echo '##############################################'
+    echo '##############################################'
+    echo ROS installed! Source ~/.bashrc to get in PATH.
+    echo '##############################################'
+    echo '##############################################'
+
+    pushd "$(dirname $VIRTUAL_ENV)/simulation_ws"
+    rosdep install --from-paths src --ignore-src -r -y
+    popd
 fi
 rosdep update
 
-[ -z "$VIRTUAL_ENV" ] && { echo Must run from venv! Create one in repo root and activate.; exit 2; }
 require_file="$(dirname $VIRTUAL_ENV)/simulation_ws/scripts/requirements.txt"
 [ ! -f $require_file ] && { echo "Requirements not found! Is venv in repo root?"; exit 3; }
 pip install -r $require_file
